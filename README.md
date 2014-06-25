04e93190dd333f2d5120972a3f63128e779354bc
John Hermiz <jthermiz@gmail.com>
2014-06-25 14:33:04 -0700

    Added documentation and user input feature

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 Data-Explorer
 =============
+Author: John Hermiz
+Last Data Modified: 6/25/14
 
 Tool built originally for Intan data exploration
+
+
+%% Section 1, Getting Started
+
+run 'Run_Intan_Data_Explorer.m'
+
+A dialog window will appear. If you are importing an Intan file, press 'Yes'.
+If not, press 'No'.
+
+Press 'No' only if you have a data structure already in the Matlab workspace 
+called 'D' that has fields:
+
+amplifier_channels
+amplifier_data
+file
+frequency_parameters
+notes
+path
+spike_triggers
+t_amplifier
+
+To see a sample data strucutre 'D', load 'D_sample'
+
+
--- Run_Intan_Data_Explorer.m
+++ Run_Intan_Data_Explorer.m
@@ -1,15 +1,28 @@
 %Run Intan Data Explorer
 %Helper script that import data and runs GUI
 
-clear
-read_Intan_RHD2000_file; %runs Intan Matlab import script
-%save('IntanData');
-D.amplifier_channels=amplifier_channels;
-D.amplifier_data=amplifier_data;
-D.file=file;
-D.frequency_parameters=frequency_parameters;
-D.notes=notes;
-D.path=path;
-D.spike_triggers=spike_triggers;
-D.t_amplifier=t_amplifier;
+choice=questdlg('Import Intan Data?','Yes','No');
+
+if strcmp(choice,'Yes')
+    clear
+    read_Intan_RHD2000_file; %runs Intan Matlab import script
+    %save('IntanData');
+    D.amplifier_channels=amplifier_channels;
+    D.amplifier_data=amplifier_data;
+    D.file=file;
+    D.frequency_parameters=frequency_parameters;
+    D.notes=notes;
+    D.path=path;
+    D.spike_triggers=spike_triggers;
+    D.t_amplifier=t_amplifier;
+elseif strcmp(choice,'No')
+    if exist('D','var')==0
+        errordlg('No variable D defined in workspace! Aborting');
+        return;
+    end
+    
+elseif strcmp(choice,'Cancel')
+    return;
+end
+
 Intan_Data_Explorer('UserData',D);
